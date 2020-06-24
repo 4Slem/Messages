bcf61729ea73e8056dcaac54913ecb9ff19f8b58
9987895937 <9987895937@koji-projects.com>
2020-06-24 09:48:50 +0000

    change package.json

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { InstantRemixing, FeedSdk } from '@withkoji/vcc';
 
 class App extends React.Component {
+  state = {
+    userName: 'sdfsd'
+  }
+
+  constructor(props) {
+    super(props);
+    this.instantRemixing = new InstantRemixing();
+    this.instantRemixing.onSetRemixing((isRemixing) => {
+      this.isRemixing = isRemixing;
+      console.log('this.isRemixing', this.isRemixing)
+    });
+
+    this.instantRemixing.ready();
+    this.feed = new FeedSdk();
+    this.feed.load();
+  }
+
+  componentDidMount() {
+    console.log(this.instantRemixing.get(['userSettings', 'userName']), 'sdfdsf')
+ this.instantRemixing.onValueChanged((path, newValue) => {
+   console.log('sdfsd')
+      this.setState({
+        userName: this.instantRemixing.get(['userSettings', 'userName'])
+      });
+    })
+  }
+
   render() {
     return (
-      <div>Hello Koji</div>
+      <div onClick={() => this.instantRemixing.onPresentControl(['userSettings', 'userName'])}>{ this.state.userName }</div>
     );
   }
 }
