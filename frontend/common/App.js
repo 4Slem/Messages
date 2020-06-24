131ce627c95c354524767f11c36015d4c7737a23
Roman Vakulin <roman.vakulin@otakoyi.com.ua>
2020-06-24 10:57:00 +0300

    clean project

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -1,30 +1,9 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-    background-color: #395b88;
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    font-size: calc(10px + 2vmin);
-    text-align: center;
-    color: #fcfcfc;
-`;
-
-const Image = styled.img`
-    max-width: 50vmin;
-    max-height: 50vmin;
-`;
 
 class App extends React.Component {
   render() {
     return (
-      <Container>
-        <h1>{'Welcome to Your App'}</h1>
-        <Image src={'https://images.koji-cdn.com/d9c6b38e-08c4-4faf-ae8e-01082c41a0fb/3f83q-9634d620e97345a6b250ca2feb7e5a2e.png'} />
-      </Container>
+    <div>Hello Koji</div>
     );
   }
 }
