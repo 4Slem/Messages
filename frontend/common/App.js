ec9c34794b974b48028542fd40e729664319dcfe
9987895937 <9987895937@koji-projects.com>
2020-06-24 21:36:06 +0000

    fix

--- frontend/common/App.js
+++ frontend/common/App.js
@@ -49,7 +49,8 @@ class App extends React.Component {
   }
 
   initMessages() {
-    this.props.addMessages(this.props.vcc.instantRemixing.get(['messagesSettings', 'messages']));
+    console.log('dfssd', this.props.vcc.instantRemixing.get(['userSettings']))
+    // this.props.addMessages(this.props.vcc.instantRemixing.get(['messagesSettings', 'messages']));
   }
 
   render() {
