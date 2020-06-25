c92a9f9b28da299bc941fa6626cc93114bf220ed
9987895937 <9987895937@koji-projects.com>
2020-06-25 19:11:48 +0000

    fix

--- frontend/common/components/message/index.js
+++ frontend/common/components/message/index.js
@@ -1,16 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './index.scss';
 
 const Message = ({ data, click }) => {
+    const [animation, setAnimation] = useState(false);
+    setTimeout(() => {
+        setAnimation(true);
+    }, 10)
+  
     if (data.imgSrc) {
         return (
-            <div className={`message-img ${data.direction === 'receiver' ? 'receiver' : ''}`} onClick={click}>
+            <div className={`message-img ${data.direction === 'receiver' ? 'receiver' : ''} ${animation && 'animation'}`} onClick={click}>
                 <img src={data.imgSrc} />
             </div>
         );
     }
     return (
-        <div className={`message ${data.direction === 'receiver' ? 'receiver' : ''}`} onClick={click}>
+        <div className={`message ${data.direction === 'receiver' ? 'receiver' : ''} ${animation && 'animation'}`} onClick={click}>
             {data.message}
         </div>
     );
