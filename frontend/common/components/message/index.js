3fef422569d24ec25293662da31764acab814512
9987895937 <9987895937@koji-projects.com>
2020-06-25 19:37:34 +0000

    fix

--- frontend/common/components/message/index.js
+++ frontend/common/components/message/index.js
@@ -1,22 +1,27 @@
 import React, { useState } from 'react';
 import './index.scss';
+import arrowIcon from '../../assets/icons/ar.svg';
+import arrowIcon2 from '../../assets/icons/ar2.svg';
 
 const Message = ({ data, click }) => {
     const [animation, setAnimation] = useState(false);
     setTimeout(() => {
         setAnimation(true);
-    }, 10)
+    }, 10);
+
+    const isReceiver = data.direction === 'receiver';
   
     if (data.imgSrc) {
         return (
-            <div className={`message-img ${data.direction === 'receiver' ? 'receiver' : ''} ${animation && 'animation'}`} onClick={click}>
-                <img src={data.imgSrc} />
+            <div className={`message-img ${isReceiver && 'receiver'} ${animation && 'animation'}`} onClick={click}>
+                <img className="message-img__image" src={data.imgSrc} />
             </div>
         );
     }
     return (
-        <div className={`message ${data.direction === 'receiver' ? 'receiver' : ''} ${animation && 'animation'}`} onClick={click}>
+        <div className={`message ${isReceiver && 'receiver'} ${animation && 'animation'}`} onClick={click}>
             {data.message}
+            <img className="message__arrow" src={isReceiver ? arrowIcon2 : arrowIcon} />
         </div>
     );
 };
