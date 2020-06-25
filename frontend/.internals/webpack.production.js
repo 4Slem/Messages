a7e14bc39edbed1a6341c7d255f4a92725961995
9987895937 <9987895937@koji-projects.com>
2020-06-25 22:46:26 +0000

    fixx

--- frontend/.internals/webpack.production.js
+++ frontend/.internals/webpack.production.js
@@ -139,6 +139,16 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       minify: {
+        removeComments: true,
+        collapseWhitespace: true,
+        removeRedundantAttributes: true,
+        useShortDoctype: true,
+        removeEmptyAttributes: true,
+        removeStyleLinkTypeAttributes: true,
+        keepClosingSlash: true,
+        minifyJS: true,
+        minifyCSS: true,
+        minifyURLs: true,
       },
       inject: true,
       template: './common/index.html',
